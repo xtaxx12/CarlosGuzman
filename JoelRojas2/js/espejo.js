9cf2a252f05fc025016dc44aff213cfeb251c41e
xtaxx12 <rojassebas765@gmail.com>
2023-05-13 12:38:16 -0500

    hola

--- JoelRojas2/js/espejo.js
+++ JoelRojas2/js/espejo.js
@@ -11,3 +11,12 @@ if (navigator.mediaDevices.getUserMedia) {
     console.log("Algo salio mal");
   });
 }
+const stars = document.querySelectorAll('.stars i');
+
+stars.forEach((star, index1) => {
+  star.addEventListener('click', () => {
+    stars.forEach((star, index2) => {
+      index1 >= index2 ? star.classList.add('active') : star.classList.remove('active');
+    });
+  });
+});
